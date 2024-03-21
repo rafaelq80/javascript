32e324462247c1992f2354f913309a4c53e93d4d
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-03-21 15:40:24 -0300

    Update

---
+++ aula_03/Exe04.js
@@ -0,0 +1,50 @@
+﻿const leia = require('readline-sync');
+
+let palavra1, palavra2, palavra3;
+
+palavra1 = leia.question("Digite a primeira palavra: ");
+palavra2 = leia.question("Digite a segunda palavra: ");
+palavra3 = leia.question("Digite a terceira palavra: ");
+
+if (palavra1 === "vertebrado"){
+// vertebrado
+    if(palavra2 === "ave"){
+        // ave
+        if(palavra3 === "carnivoro"){
+            console.log("Águia");
+        }else{
+            console.log("Pomba");
+        }
+
+    }else{
+        // mamifero
+
+        if(palavra3 === "herbivoro"){
+            console.log("Vaca");
+        }else{
+            console.log("Homem");
+        }
+
+    }
+
+}else{
+// Invertebrado
+
+    if(palavra2 === "inseto"){
+
+        if( palavra3 === "herbivoro"){
+            console.log("Lagarta");
+        }else{
+            console.log("Pulga");
+        }
+
+    }else{
+
+        if( palavra3 === "onivoro"){
+            console.log("Minhoca");
+        }else{
+            console.log("Sanguessuga");
+        }
+    }
+}
+
---
+++ aula_03/Exe08.js
@@ -0,0 +1,42 @@
+﻿const leia = require('readline-sync');
+
+let valor, saldo = 1000.00, operacao;
+
+console.log("Informe a Operação");
+console.log("1 - Saldo");
+console.log("2 - Saque");
+console.log("3 - Depósito");
+operacao = leia.questionInt("Digite a opcao desejada: ");
+
+switch(operacao){
+    case 1:
+        console.log("Saldo");
+
+        console.log(`\nO Saldo da conta é: ${saldo.toFixed(2)}`);
+    break;
+
+    case 2:
+        console.log("Saque");
+
+        valor = leia.questionFloat("\nDigite o valor: ")
+
+        if (saldo > valor)
+            console.log(`\n O novo saldo é: ${ (saldo - valor).toFixed(2) }`);
+        else
+            console.log("\nSaldo é Insuficiente!");
+        
+
+    break;
+
+    case 3:
+        console.log("Depósito");
+
+        valor = leia.questionFloat("\nDigite o valor: ")
+
+        console.log(`\n O novo saldo é: ${ (saldo + valor).toFixed(2) }`);
+
+    break;
+
+    default:
+        console.log("Operação Inválida!");
+}
